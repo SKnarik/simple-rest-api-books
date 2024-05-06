05a69d80f116c77d8bd3c496455aca34782c7b57
KnarikSoghoyan <knariksoghoyan@gmail.com>
2024-05-06 11:00:46 +0400

    Added Nodemon, Prettier, FS Functionality, and Books JSON Integration

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import http from 'http';
+import http from "http";
+import fs from "fs";
 
-http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify({message:"Hello world!"}));
-        res.end()
-    }
-    else
-        res.end("Invalid request!")
-
-}).listen(3000)
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(fs.readFileSync("./books.json", "utf-8"));
+      res.end();
+    } else res.end("Invalid request!");
+  })
+  .listen(3000);
 console.log("Server running at port 3000");
